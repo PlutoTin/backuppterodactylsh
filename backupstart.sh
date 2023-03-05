b23031183a4796474be12cd4b09c8ffc59891962
PlutoTin <93427189+PlutoTin@users.noreply.github.com>
2023-03-05 09:47:05 +0800

    Add files via upload

--- backupstart.sh
+++ backupstart.sh
@@ -1,62 +1,103 @@
-#!/bin/bash
+﻿#!/bin/bash
 
-if (( $EUID != 0 )); then
-    echo "Please run as root"
-    exit
-fi
 
-clear
-
-installTheme(){
-    echo "backup..."
-    cd /var/www/
-    tar -czvf backup.tar.gz pterodactyl
-    cd /var/www/../..
-    
-    
+backupwd() {
+    cd /var/www/pterodactyl && php artisan down && cd /var/www/ && tar -czvf backup.tar.gz /var/www/pterodactyl && cd /var/www/pterodactyl && php artisan up && cd /var/www/pterodactyl/../../..
 }
 
-installThemeQuestion(){
-    while true; do
-        read -p "Are you sure that you want to install the theme [y/n]? " yn
-        case $yn in
-            [Yy]* ) installTheme; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
+
+restorewd() {
+    cd /var/www/pterodactyl && php artisan down && cd /var/www/ && tar -xzvf backup.tar.gz -C /var/www && cd /var/www/../.. && cd /var/www/pterodactyl && php artisan up && cd /var/www/pterodactyl/../../..
 }
 
-restoreBackUp(){
-    echo "Restoring backup..."
-    cd /var/www/
-    tar -xzvf MinecraftPurpleThemebackup.tar.gz
-    cd /var/www/../..
 
+
+
+backupwod() {
+    cd /var/www/pterodactyl && cd /var/www/ && tar -czvf backup.tar.gz /var/www/pterodactyl && cd /var/www/pterodactyl && cd /var/www/pterodactyl/../../..
+}
+
+
+restorewod() {
+    cd /var/www/pterodactyl && cd /var/www/ && tar -xzvf backup.tar.gz -C /var/www && cd /var/www/../.. && cd /var/www/pterodactyl && cd /var/www/pterodactyl/../../..
+}
+
+
+backuplocal() {
     cd /var/www/pterodactyl
-    yarn build:production
-    sudo php artisan optimize:clear
+    php artisan down
+    read -p "Please enter the IP address: " ip_address
+    read -p "Please enter the download location: " download_location
+    scp -r root@$ip_address:/var/www/pterodactyl $download_location
+    php artisan up
 }
 
-echo "This program is modify from https://github.com/Angelillo15/MinecraftPurpleTheme"
-echo "[1] Install theme"
-echo "[2] Restore backup
-echo "[4] Exit"
+
+backuptargzlocal() {
+    cd /var/www/pterodactyl
+    php artisan down
+    read -p "Please enter the IP address: " ip_address
+    read -p "Please enter the download location: " download_location
+    tar -czvf backuplocal.tar.gz ./*
+    scp -r backuplocal.tar.gz root@$ip_address:$download_location
+    rm backuplocal.tar.gz
+    php artisan up
+}
+
+
+
+
+
+
+echo "[1] Backup with panel down"
+echo "[2] Restore with panel down"
+echo "[3] Backup without panel down"
+echo "[4] Restore without panel down"
+echo "[5] Backup local in computer (download)"
+echo "[6] Backup local in computer (zip and download)"
+echo "[7] Exit"
+
 
 read -p "Please enter a number: " choice
+
+
 if [ $choice == "1" ]
-    then
-    installThemeQuestion
+then
+    backupwd
 fi
+
+
 if [ $choice == "2" ]
-    then
-    restoreBackUp
+then
+    restorewd
 fi
+
+
 if [ $choice == "3" ]
-    then
-    repair
+then
+    backupwod
 fi
+
+
 if [ $choice == "4" ]
-    then
+then
+    restorewod
+fi
+
+
+if [ $choice == "5" ]
+then
+    backuplocal
+fi
+
+
+if [ $choice == "6" ]
+then
+    backuptargzlocal
+fi
+
+
+if [ $choice == "7" ]
+then
     exit
 fi
