9b1106eaa37f76b82f985c57a58b344c10090cb5
PlutoTin <93427189+PlutoTin@users.noreply.github.com>
2023-03-05 12:37:46 +0800

    Add files via upload

---
+++ backupstart.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+clear
+varwww(){
+bash <(curl https://raw.githubusercontent.com/PlutoTin/backuppterodactylsh/main/backupwwwstart.sh)
+}
+varlib(){
+bash <(curl https://raw.githubusercontent.com/PlutoTin/backuppterodactylsh/main/backuplibstart.sh)
+}
+echo "[1] Backup or restore /var/www/pterodactyl"
+echo "[2] Backup or restore /var/lib/pterodactyl"
+echo "[3] Exit"
+read -p "Please enter a number: " choice
+if [ "$choice" == "1" ]
+then
+    varwww
+fi
+if [ "$choice" == "2" ]
+then
+    varlib
+fi
+if [ "$choice" == "3" ]
+then
+    exit
+fi
