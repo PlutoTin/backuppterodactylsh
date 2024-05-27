198b74e7f45c5dce38956b35c2d5a9f18d21964a
PlutoTin <93427189+PlutoTin@users.noreply.github.com>
2024-05-27 18:29:50 +0800

    Create repair.sh

---
+++ helpingtools/repair.sh
@@ -0,0 +1,39 @@
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+repairPanel(){
+    cd /var/www/pterodactyl
+
+    php artisan down
+
+    rm -r /var/www/pterodactyl/resources
+
+    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+
+    chmod -R 755 storage/* bootstrap/cache
+
+    composer install --no-dev --optimize-autoloader
+
+    php artisan view:clear
+
+    php artisan config:clear
+
+    php artisan migrate --seed --force
+
+    chown -R www-data:www-data /var/www/pterodactyl/*
+
+    php artisan queue:restart
+
+    php artisan up
+}
+
+while true; do
+    read -p "Are you sure that you want to auto repair? [y/n]? " yn
+    case $yn in
+        [Yy]* ) repairPanel; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
